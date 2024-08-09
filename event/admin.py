ce1cb8721f53a87c4f0a8d4a3fdd80d34712efa6
Viesich <v.s.vesich@gmail.com>
2024-08-09 19:58:42 +0300

    add class EventAdmin to admin.py

--- event/admin.py
+++ event/admin.py
@@ -1,3 +1,20 @@
-from django.contrib import admin
+from tkinter import EventType
 
-# Register your models here.
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+
+from event.models import Event, EventRegistration, Result, Runner
+
+
+admin.site.register(EventRegistration)
+
+admin.site.register(Result)
+
+admin.site.register(Runner, UserAdmin)
+
+
+@admin.register(Event)
+class EventAdmin(admin.ModelAdmin):
+    list_display = ("date", "event_type", )
+    search_fields = ('name',)
+    list_filter = ('name', "date", "event_type")
