8623821b7cf5b219beddfcce21e8009c02df5483
Viesich <v.s.vesich@gmail.com>
2024-09-17 15:32:08 +0300

    add staticfiles to gitignore

--- runner_community/settings.py
+++ runner_community/settings.py
@@ -145,7 +145,7 @@ USE_TZ = False
 
 STATIC_URL = "/static/"
 
-STATICFILES_DIRS = [BASE_DIR / "static/assets"]
+STATICFILES_DIRS = [BASE_DIR / "static"]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
