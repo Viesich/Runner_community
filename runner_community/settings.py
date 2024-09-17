5dd11da5f2ae543668834da0fad59ceadfada392
Viesich <v.s.vesich@gmail.com>
2024-09-17 13:17:45 +0300

    update DATABASE in settings

--- runner_community/settings.py
+++ runner_community/settings.py
@@ -94,12 +94,11 @@ WSGI_APPLICATION = "runner_community.wsgi.application"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    'default': dj_database_url.config(
+        default=os.getenv('DATABASE_URL'),
+        conn_max_age=500
+    )
 }
-
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES["default"].update(db_from_env)
 
